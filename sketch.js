cbd3463442119f228e0ff7c70afd6c0e0ae70cf7
simranlalchandani <73645763+simranlalchandani@users.noreply.github.com>
2021-01-21 16:56:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,45 +1,21 @@
-var hball,database
-var position
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+
+var database;
+
+var form, player, game;
+
 
 function setup(){
-    database=firebase.database()
-    createCanvas(500,500);
-    hball = createSprite(250,250,10,10);
-    hball.shapeColor = "red";
-
-    var hballPosition=database.ref('ball/position')
-    hballPosition.on("value",readPosition,showError)
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
 }
+
 
 function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        writePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
-    }
-    drawSprites();
-}
-
-function writePosition(x,y){
-    database.ref('ball/position').set({
-        'x':position.x+x,
-        'y':position.y+y
-        
-    })
-}
-function readPosition(data){
-    position=data.val()
-    hball.x=position.x
-    hball.y=position.y
-}
-function showError(){
-    console.log("error in writing to the database")
 }
